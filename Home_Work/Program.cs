26f6a5851146e37ce9b12ab8be73853194897bf5
Aleksei Vaskov <prizrak1143@gmail.com>
2023-02-08 16:16:59 +0300

    этап 3

--- Home_Work/Program.cs
+++ Home_Work/Program.cs
@@ -3,8 +3,10 @@
 
 Console.WriteLine("Пожалуйста введите число элементов");
 int ColElement = Convert.ToInt32(Console.ReadLine());
-string[] Massiv = new string [ColElement];
-int ColPerRav3 = 0;
+string[] Massiv = new string[ColElement];
+int ColPerProm03 = 0;
+string Text = null;
+int ColVhod = 0;
 
 for (int i = 0; i < ColElement; i++)
 {
@@ -16,10 +18,14 @@ var str = string.Join(" ", Massiv);
 Console.WriteLine(str);
 
 
-
-
-
-
+for (int i = 0; i < ColElement; i++)
+{
+    Text = Massiv[i];
+    if (Text.Length < 4 && Text.Length >= 0)
+    {
+        ColPerProm03 = ColPerProm03 + 1;
+    }
+}
 
 
 
